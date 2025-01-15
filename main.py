79208b933e1cbc29aef12058f055641e7168cdd1
paxington181 <tsandrus@rogers.com>
2025-01-15 17:33:22 -0500

    Filtered to remove line returns, start of sorting added.

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ def main():
     split_text = text_splitter(book_text) 
     word_counter = list_len(split_text)
     lower_case_dictionary = l_case_dict(book_text)
+    percent_alphabet = "place holder"
+
 
     print(f"Review of text begins")
     print(f"The book contained in {bookfile} contains {word_counter} words.")
+    print(f"{percent_alphabet}% of the characters in {bookfile} are letters.")
     print(lower_case_dictionary)
+
 
 def get_book(path):
     with open(path) as f:
@@ -26,10 +30,15 @@ def l_case_dict(book_text):
     letter_list = list(l_case)
     dictionary = {}
     for let in letter_list:
+        if let == "\n":
+            continue        
         if let in dictionary:
             dictionary[let] += 1
         else:
             dictionary[let] = 1
     return dictionary
+
+def sort_on(l_case_dict):
+    return l_case_dict["num"]
 
 main()
